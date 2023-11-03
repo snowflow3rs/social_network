fee6c5857627f797a1670cc81fe6bd09b11b9f30
Snow flowers <zero2392001@gmail.com>
2023-11-03 12:15:13 +0700

    redevelope and first setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+export const metadata = {
+  title: "social",
+  description: "A social network",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+    <html lang="en-vi">
+        <body className={`${inter.className}  bg-dark-1`}>
+          <div className=" w-full flex justify-center items-center  min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
