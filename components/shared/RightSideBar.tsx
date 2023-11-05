2635e33f95c1bc8987d22f9cf6a6070b241f2056
Snow flowers <zero2392001@gmail.com>
2023-11-05 09:25:39 +0700

    Update UI for right side bar

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,49 +1,74 @@
-// import { fetchUser, fetchUsersRightBar } from "@/lib/actions/user.action";
-// import React from "react";
-// import { currentUser } from "@clerk/nextjs";
-// import { fetchCommunities } from "@/lib/actions/community.action";
-// import UserCard from "../cards/UserCard";
-const RightSideBar: React.FC = async () => {
-  // const user = await currentUser();
-  // if (!user) return null;
+import { currentUser } from "@clerk/nextjs";
 
-  // const similarMinds = await fetchUsersRightBar({
-  //   userId: user.id,
-  //   pageSize: 4,
-  // });
+import UserCard from "../cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUsersRightBar } from "@/lib/actions/user.actions";
 
-  // const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
+
+
+async function RightSidebar() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const similarMinds = await fetchUsersRightBar({
+    userId: user.id,
+    pageSize: 4,
+  });
+
+  const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
+
   return (
-    <section className="custom-scrollbar rightsidebar">
-      <div className=" flex flex-1 flex-col justify-start">
-        <h3 className="text-light-1 text-heading4-medium">
-          Suggested Community
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
         </h3>
+
+        <div className='mt-7 flex w-[350px] flex-col gap-9'>
+          {suggestedCOmmunities.communities.length > 0 ? (
+            <>
+              {suggestedCOmmunities.communities.map((community) => (
+                <UserCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  userType='Community'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>
+              No communities yet
+            </p>
+          )}
+        </div>
       </div>
 
-      <div className=" flex flex-1 flex-col justify-start">
-        <h3 className="text-light-1 text-heading4-medium">Suggested Users</h3>
-        <div className="mt-7 flex w-[350px] flex-col gap-10">
-          {/* {similarMinds.users.length > 0 ? (
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+          {similarMinds.users.length > 0 ? (
             <>
-              {similarMinds.users.map((person: any) => (
+              {similarMinds.users.map((person:any) => (
                 <UserCard
                   key={person.id}
                   id={person.id}
                   name={person.name}
                   username={person.username}
                   imgUrl={person.image}
-                  userType="User"
+                  userType='User'
                 />
               ))}
             </>
           ) : (
-            <p className="!text-base-regular text-light-3">No users yet</p>
-          )} */}
+            <p className='!text-base-regular text-light-3'>No users yet</p>
+          )}
         </div>
       </div>
     </section>
   );
-};
+}
 
-export default RightSideBar;
+export default RightSidebar;
