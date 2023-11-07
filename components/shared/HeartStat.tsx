6be647f6b90436800319f7e8afb9c722ed1e84e1
Snow flowers <zero2392001@gmail.com>
2023-11-07 08:01:37 +0700

    Fixing error boundary and add edit btn for threads

--- components/shared/HeartStat.tsx
+++ components/shared/HeartStat.tsx
@@ -1,56 +1,50 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { checkIsLiked } from "@/lib/utils";
-import { likePosts } from "@/lib/actions/thread.actions";
+import React, { useState } from 'react';
+import { checkIsLiked } from '@/lib/utils';
+import { likePosts } from '@/lib/actions/thread.actions';
 
 interface Props {
-  postId: string;
-  likes: string[];
+    postId: string;
+    likes: string[];
 
-  userId: string;
+    userId: string;
 }
 const HeartStat = ({ postId, likes, userId }: Props) => {
-  const [liked, setLiked] = useState<string[]>(likes);
+    const [liked, setLiked] = useState<string[]>(likes);
 
-  const handleLikePost = (
-    e: React.MouseEvent<HTMLImageElement, MouseEvent>
-  ) => {
-    e.stopPropagation();
+    const handleLikePost = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+        e.stopPropagation();
 
-    let newLiked = [...liked];
+        let newLiked = [...liked];
 
-    if (newLiked.includes(JSON.parse(userId))) {
-      newLiked = newLiked.filter((Id) => Id !== JSON.parse(userId));
-    } else {
-      newLiked.push(JSON.parse(userId));
-    }
+        if (newLiked.includes(JSON.parse(userId))) {
+            newLiked = newLiked.filter((Id) => Id !== JSON.parse(userId));
+        } else {
+            newLiked.push(JSON.parse(userId));
+        }
 
-    setLiked(newLiked);
-    likePosts(JSON.parse(postId), newLiked);
-  };
+        setLiked(newLiked);
+        likePosts(JSON.parse(postId), newLiked);
+    };
 
-  return (
-    <div className="flex group transition-colors">
-      <div className=" rounded-lg group-hover:bg-red-400">
-        <img
-          src={`${
-            checkIsLiked(liked, JSON.parse(userId))
-              ? "/assets/liked.svg"
-              : "/assets/heart-gray.svg"
-          }`}
-          alt="heart"
-          width={24}
-          height={24}
-          onClick={handleLikePost}
-          className="cursor-pointer object-contain"
-        />
-      </div>
-      <p className="small-medium lg:base-medium cursor-pointer text-gray-1 ml-[2px] group-hover:text-red-400">
-        {liked?.length}
-      </p>
-    </div>
-  );
+    return (
+        <div className="flex group transition-colors">
+            <div className=" rounded-lg group-hover:bg-red-400">
+                <img
+                    src={`${checkIsLiked(liked, JSON.parse(userId)) ? '/assets/liked.svg' : '/assets/heart-gray.svg'}`}
+                    alt="heart"
+                    width={24}
+                    height={24}
+                    onClick={handleLikePost}
+                    className="cursor-pointer object-contain"
+                />
+            </div>
+            <p className="small-medium lg:base-medium cursor-pointer text-gray-1 ml-[2px] group-hover:text-red-400">
+                {liked?.length}
+            </p>
+        </div>
+    );
 };
 
 export default HeartStat;
